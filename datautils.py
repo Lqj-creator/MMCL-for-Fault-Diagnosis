c573e4d307413e5b51377a989686e93b7ee0f0d1
Liang Qiujin <72275543+Lqj-creator@users.noreply.github.com>
2024-11-26 14:41:02 +0800

    Update datautils.py

--- datautils.py
+++ datautils.py
@@ -53,6 +53,29 @@ def load_Diagnosis(dataset):
     
     return train[..., np.newaxis], train_labels, test[..., np.newaxis], test_labels
 
+def load_PHM_finetune(dataset, num_perclass):
+    train_file = os.path.join('datasets/PHM/train/')
+    test_file = os.path.join('datasets/PHM/test/')
+    train = np.load(train_file + 'train_dataset.npy')
+    train_labels = np.load(train_file + 'train_label.npy')
+
+    pre_num = train.shape[0]
+
+    indices = np.arange(10)
+    length = int(train.shape[0])
+    length_per = int(length/10)
+    length_new = int(length*num_perclass/10)
+    data_new = [train[each*length_per:(each*length_per+length_new),:] for each in indices]
+    label_new = [train_labels[each*length_per:(each*length_per+length_new)] for each in indices]
+    train = np.concatenate(data_new)
+    train_labels = np.concatenate(label_new)
+    
+    finetune_num = train.shape[0]
+
+    test = np.load(test_file + 'test_dataset.npy')
+    test_labels = np.load(test_file + 'test_label.npy')
+    test_num = test.shape[0]
+    return train, train_labels, test, test_labels, pre_num, finetune_num, test_num
 
 
 
